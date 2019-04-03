ea508248cc08ad65dde378f5da6a952838914d80
sasoria <sergio_s86@hotmail.com>
2019-04-03 15:49:56 +0200

    Added node-service

--- node-service/app/js/app.js
+++ node-service/app/js/app.js
@@ -1,1 +1,25 @@
-app.js
+var app = angular.module("app", ['ngResource', 'ngRoute']);
+
+app.controller("AppController", function($scope, $http) {
+	$scope.info = "insert info here";
+
+	$http.get('http://api.bf4stats.com/api/playerInfo?plat=pc&name=1ApRiL&output=json')
+	.then(successCallback, errorCallback);
+
+	function successCallback(response){
+    	console.log(response);
+    	$scope.player = response.data.player.name;
+	}
+
+	function errorCallback(error){
+    	console.log(error);
+	}
+});
+
+app.controller('AppCtrl', function($scope) {
+	$scope.message = "The app routing is working!";
+});
+
+app.controller('UserCtrl', function($scope) {
+ 	$scope.message = "The app routing is still working!";
+});
