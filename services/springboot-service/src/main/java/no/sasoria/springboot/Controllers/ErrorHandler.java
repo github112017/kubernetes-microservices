6321f86604104063a9d4466cfae68e19b7154058
sasoria <sergio_s86@hotmail.com>
2019-04-16 10:57:16 +0200

    Added error handler

---
+++ services/springboot-service/src/main/java/no/sasoria/springboot/Controllers/ErrorHandler.java
@@ -0,0 +1,39 @@
+package no.sasoria.springboot.Controllers;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ RuntimeException.class })
+    protected ResponseEntity<Object> handleGenericError(RuntimeException e, WebRequest request) {
+        return handleException(e, request, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({ IllegalArgumentException.class })
+    protected ResponseEntity<Object> handleBadRequest(IllegalArgumentException e, WebRequest request) {
+        return handleException(e, request, HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<Object> handleException(final RuntimeException e, WebRequest request,
+                                                   HttpStatus httpStatus) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("errorMessage", e.getMessage());
+        if (e.getCause() != null) {
+            error.put("cause", e.getCause().getMessage());
+        }
+        return handleExceptionInternal(e, error, headers, httpStatus, request);
+    }
+}
