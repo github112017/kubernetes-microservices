9988fc21e5f59f51df5fe866ae7deb2d821e7ed5
sasoria <sergio_s86@hotmail.com>
2019-04-16 15:47:31 +0200

    Create redeploy.sh
    Added redeploy script

---
+++ redeploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ROOT_DIR=$(pwd)
+SPRINGBOOT_DIR=$ROOT_DIR/services/springboot-service
+NODE_DIR=$ROOT_DIR/services/node-service
+
+# node
+cd $NODE_DIR
+echo "[log] deleting node-service..."
+kubectl delete -f application.yml
+
+echo "[log] creating node-service..."
+sleep 4
+kubectl create -f application.yml
+
+# springboot
+cd $SPRINGBOOT_DIR
+echo "[log] deleting springboot-service..."
+kubectl delete -f application.yml
+
+echo "[log] deleting springboot-service..."
+sleep 4
+kubectl create -f application.yml
