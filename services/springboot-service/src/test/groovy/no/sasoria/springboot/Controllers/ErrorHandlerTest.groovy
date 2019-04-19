0cc3eb97adb55a099b9887a39813f78e1d107fb0
sasoria <sergio_s86@hotmail.com>
2019-04-19 21:53:33 +0200

    Added groovy test structure

---
+++ services/springboot-service/src/test/groovy/no/sasoria/springboot/Controllers/ErrorHandlerTest.groovy
@@ -0,0 +1,36 @@
+package no.sasoria.springboot.Controllers
+
+import org.springframework.http.HttpStatus
+import spock.lang.Specification
+
+class ErrorHandlerTest extends Specification {
+
+    public static final String ERROR_MESSAGE = "Test"
+    public static final String ROOT_CAUSE = "Root cause"
+    def errorHandler = new ErrorHandler()
+
+    def "Sets correct status code and error message on causeless Exceptions"() {
+
+        when:
+        def request = errorHandler.handleBadRequest(new IllegalArgumentException(ERROR_MESSAGE), null)
+
+        then:
+        request.statusCode == HttpStatus.BAD_REQUEST
+        request.body.errorMessage == ERROR_MESSAGE
+        !request.body.cause
+    }
+
+    def "Sets cause on Exceptions with a root cause"() {
+
+        given:
+        IllegalArgumentException e = new IllegalArgumentException(ERROR_MESSAGE, new IndexOutOfBoundsException(
+                ROOT_CAUSE))
+        when:
+        def request = errorHandler.handleBadRequest(e, null)
+
+        then:
+        request.statusCode == HttpStatus.BAD_REQUEST
+        request.body.errorMessage == ERROR_MESSAGE
+        request.body.cause == ROOT_CAUSE
+    }
+}
