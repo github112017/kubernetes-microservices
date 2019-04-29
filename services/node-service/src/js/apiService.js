6ab9dc67db5aa9a0e27cf4925e43529b3b13aaf6
sasoria <sergio_s86@hotmail.com>
2019-04-29 22:22:09 +0200

    Refactored code

--- services/node-service/src/js/apiService.js
+++ services/node-service/src/js/apiService.js
@@ -1,27 +1,24 @@
 app.factory("apiService", function($http, $q) {
-  var base_url = 'http://minikube:30003'
+  /* ExpressJS server url, using nodeport 30003 */
+  var baseUrl = 'http://minikube:30003'
+  var playerApi = '/api/player/'
+
 
   return {
-    load_player: function(name){
-      var api_load = '/api/player/'
-      console.log("sending $http.get to : " + base_url+api_load+name)
-
+    load_player: function(name) {
       return $http({
         method: 'PUT',
-        url: base_url + api_load + name,
+        url: baseUrl+playerApi+name,
         headers: {
           'Content-Type': 'text/plain'
         }
       })
     },
 
-    get_player: function(name){
-      var api_get = '/api/player/'
-      console.log("sending $http.get to : " + base_url+api_get+name)
-
+    get_player: function(name) {
       return $http({
         method: 'GET',
-        url: base_url + api_get + name,
+        url: baseUrl+playerApi+name,
         headers: {
           'Content-Type': 'application/json'
         }
